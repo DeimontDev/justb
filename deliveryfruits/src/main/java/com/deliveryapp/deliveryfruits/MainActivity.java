d67238a0447c4ff5a6442cefcc2505391e80067b
DeimontDev <lbvfyq@gmail.com>
2018-10-28 20:16:14 +0200

    Added handler for plus and minus buttons, layouts.

--- deliveryfruits/src/main/java/com/deliveryapp/deliveryfruits/MainActivity.java
+++ deliveryfruits/src/main/java/com/deliveryapp/deliveryfruits/MainActivity.java
@@ -1,5 +1,6 @@
 package com.deliveryapp.deliveryfruits;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -24,15 +25,47 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        ImageButton plus1 = findViewById(R.id.plus1);
+        Resources res = getResources();
+        for (int i = 0; i < 10; i++) {
+            int id = i + 1;
+            ImageButton plus = findViewById(res.getIdentifier(
+                    "plus"+ String.valueOf(id), "id", getPackageName()));
+            plus.setOnClickListener(plusMinusListener(id, true));
+            ImageButton minus = findViewById(res.getIdentifier(
+                    "minus"+ String.valueOf(id), "id", getPackageName()));
+            minus.setOnClickListener(plusMinusListener(id, false));
+        }
 
-        plus1.setOnClickListener(new View.OnClickListener() {
+    }
+
+    private View.OnClickListener plusMinusListener(final int id, final boolean plus) {
+        return new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                TextView text1 = findViewById(R.id.text1);
-                int newText = Integer.parseInt(text1.getText().toString()) + 1;
-                text1.setText(String.valueOf(newText));
+                Resources res = getResources();
+                TextView textView = findViewById(res.getIdentifier(
+                        "text"+ String.valueOf(id), "id", getPackageName()));
+                if (plus) {
+                    setPlus(textView);
+                } else {
+                    setMinus(textView);
+                }
             }
-        });
+        };
     }
+
+    private void setPlus(TextView textView) {
+        int newText = Integer.parseInt(textView.getText().toString()) + 1;
+        textView.setText(String.valueOf(newText));
+    }
+
+    private void setMinus(TextView textView) {
+        int text = Integer.parseInt(textView.getText().toString());
+
+        if (text > 0) {
+            int newText = text - 1;
+            textView.setText(String.valueOf(newText));
+        }
+    }
+
 }
