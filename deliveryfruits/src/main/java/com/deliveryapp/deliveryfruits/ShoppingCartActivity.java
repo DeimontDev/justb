cecdca0a54c1dc3780ac215dc4a096f2bc9e088f
DeimontDev <lbvfyq@gmail.com>
2018-11-16 23:19:10 +0200

    Added first basket

--- deliveryfruits/src/main/java/com/deliveryapp/deliveryfruits/ShoppingCartActivity.java
+++ deliveryfruits/src/main/java/com/deliveryapp/deliveryfruits/ShoppingCartActivity.java
@@ -2,17 +2,13 @@ package com.deliveryapp.deliveryfruits;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 public class ShoppingCartActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping_cart);
 
         Bundle extras = getIntent().getExtras();
         String product1 = extras != null ? extras.getString("product1") : null;
@@ -20,26 +16,9 @@ public class ShoppingCartActivity extends AppCompatActivity {
 
         System.out.println(product1);
         System.out.println(product2);
-        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
-        LinearLayout layout = new LinearLayout(this);
-        layout.setOrientation(LinearLayout.VERTICAL);
 
-        setContentView(layout, layoutParams);
 
-        ViewGroup.LayoutParams lpView = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        TextView tv = new TextView(this);
-        tv.setText(product1);
-        tv.setLayoutParams(lpView);
-        layout.addView(tv);
 
-        Button btn = new Button(this);
-        btn.setText(product2);
-        layout.addView(btn, lpView);
-
-        setContentView(layout, layoutParams);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
     }
 
 }
