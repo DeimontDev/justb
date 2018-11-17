9f1543a5d9ffd5f5054a9f03d40fa4c2221dd503
DeimontDev <lbvfyq@gmail.com>
2018-11-17 23:19:22 +0200

    Added all products to basket shop and did logic with delete and go back

--- deliveryfruits/src/main/java/com/deliveryapp/deliveryfruits/MainActivity.java
+++ deliveryfruits/src/main/java/com/deliveryapp/deliveryfruits/MainActivity.java
@@ -2,9 +2,11 @@ package com.deliveryapp.deliveryfruits;
 
 import android.content.Intent;
 import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -29,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
 
         for (int i = 0; i < 20; i++) {
             int id = i + 1;
-            ImageButton toBasket = findViewById(res.getIdentifier(
+            AppCompatButton toBasket = findViewById(res.getIdentifier(
                     "button" + String.valueOf(id), "id", getPackageName()));
             toBasket.setOnClickListener(basketListener(id));
         }
@@ -37,14 +39,12 @@ public class MainActivity extends AppCompatActivity {
         for (int i = 0; i < 20; i++) {
             int id = i + 1;
             ImageButton plus = findViewById(res.getIdentifier(
-                    "plus"+ String.valueOf(id), "id", getPackageName()));
+                    "plus" + String.valueOf(id), "id", getPackageName()));
             plus.setOnClickListener(plusMinusListener(id, true));
 
             ImageButton minus = findViewById(res.getIdentifier(
-                    "minus"+ String.valueOf(id), "id", getPackageName()));
+                    "minus" + String.valueOf(id), "id", getPackageName()));
             minus.setOnClickListener(plusMinusListener(id, false));
-
-
         }
     }
 
@@ -52,15 +52,28 @@ public class MainActivity extends AppCompatActivity {
         return new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Resources res = getResources();
-                TextView productName = findViewById(res.getIdentifier(
-                        "product"+ String.valueOf(id), "id", getPackageName()));
+                view.setBackgroundColor(Color.parseColor("#FFB77E5E"));
+                TextView textView = (TextView) view;
 
-                if (intent == null) {
-                    intent = new Intent(MainActivity.this, ShoppingCartActivity.class);
+                if (textView.getText().toString().equals("В корзину")) {
+                    textView.setText("Добавлено");
+                    Resources res = getResources();
+                    TextView productName = findViewById(res.getIdentifier(
+                            "product" + String.valueOf(id), "id", getPackageName()));
+                    TextView productPrice = findViewById(res.getIdentifier(
+                            "price" + String.valueOf(id), "id", getPackageName()));
+                    if (intent == null) {
+                        intent = new Intent(MainActivity.this, ShoppingCartActivity.class);
+                    }
+
+                    intent.putExtra("product" + id, productName.getText().toString());
+                    intent.putExtra("price" + id, productPrice.getText().toString());
+                } else {
+                    view.setBackgroundColor(Color.parseColor("#FFEFD8CB"));
+                    textView.setText("В корзину");
+                    intent.removeExtra("product" + id);
+                    intent.removeExtra("price" + id);
                 }
-
-                intent.putExtra("product" + id, productName.getText().toString());
             }
         };
     }
@@ -71,7 +84,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Resources res = getResources();
                 TextView textView = findViewById(res.getIdentifier(
-                        "text"+ String.valueOf(id), "id", getPackageName()));
+                        "text" + String.valueOf(id), "id", getPackageName()));
                 if (plus) {
                     setPlus(textView);
                 } else {
--- deliveryfruits/src/main/java/com/deliveryapp/deliveryfruits/ShoppingCartActivity.java
+++ deliveryfruits/src/main/java/com/deliveryapp/deliveryfruits/ShoppingCartActivity.java
@@ -1,10 +1,14 @@
 package com.deliveryapp.deliveryfruits;
 
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class ShoppingCartActivity extends AppCompatActivity {
 
@@ -13,22 +17,74 @@ public class ShoppingCartActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping_cart);
 
-        Bundle extras = getIntent().getExtras();
-        String product1 = extras != null ? extras.getString("product1") : null;
-        String product2 = extras != null ? extras.getString("product2") : null;
-
-        System.out.println(product1);
-        System.out.println(product2);
-
-        ImageButton delete1 = findViewById(R.id.delete1);
-        delete1.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton back = findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ConstraintLayout lay1 = findViewById(R.id.lay1);
-                lay1.setVisibility(View.GONE);
+                Intent intent = new Intent(ShoppingCartActivity.this, MainActivity.class);
+                startActivity(intent);
             }
         });
+        Bundle extras = getIntent().getExtras();
+
+        Resources res = getResources();
+
+        for (int i = 0; i < 20; i++) {
+            int id = i + 1;
+            ImageButton plus = findViewById(res.getIdentifier(
+                    "plus" + String.valueOf(id), "id", getPackageName()));
+            plus.setOnClickListener(plusMinusListener(id, true));
+
+            ImageButton minus = findViewById(res.getIdentifier(
+                    "minus" + String.valueOf(id), "id", getPackageName()));
+            minus.setOnClickListener(plusMinusListener(id, false));
+
+            ImageButton delete = findViewById(res.getIdentifier(
+                    "delete" + String.valueOf(id), "id", getPackageName()));
+            delete.setOnClickListener(deleteListener(id));
+        }
 
     }
 
+    private View.OnClickListener deleteListener(final int id) {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Resources res = getResources();
+                ConstraintLayout layout = findViewById(res.getIdentifier(
+                        "lay" + String.valueOf(id), "id", getPackageName()));
+                layout.setVisibility(View.GONE);
+            }
+        };
+    }
+
+    private View.OnClickListener plusMinusListener(final int id, final boolean plus) {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Resources res = getResources();
+                TextView textView = findViewById(res.getIdentifier(
+                        "text" + String.valueOf(id), "id", getPackageName()));
+                if (plus) {
+                    setPlus(textView);
+                } else {
+                    setMinus(textView);
+                }
+            }
+        };
+    }
+
+    private void setPlus(TextView textView) {
+        int newText = Integer.parseInt(textView.getText().toString()) + 1;
+        textView.setText(String.valueOf(newText));
+    }
+
+    private void setMinus(TextView textView) {
+        int text = Integer.parseInt(textView.getText().toString());
+
+        if (text > 0) {
+            int newText = text - 1;
+            textView.setText(String.valueOf(newText));
+        }
+    }
 }
